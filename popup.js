b9f0386e3f7b7ee4e28b5588fa719e8b38d597ba
Paul Woo Jung Kim <paulwoojungkim@Paului-MacBookPro.local>
2020-08-11 14:59:46 +0900

    popup.js has been added

---
+++ popup.js
@@ -0,0 +1,15 @@
+document.querySelector('.link-library').onload = () => {
+    isIframeLoaded();
+}
+
+const isIframeLoaded = () => {
+    // let iframe = document.querySelector('.link-library');
+    // let iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+    // if(iframeDoc.readyState === 'complete') {
+    //     console.log('iframe doc has been loaded')
+    //     return;
+    // }
+
+    // console.log('iframe doc is not availiable');
+}
