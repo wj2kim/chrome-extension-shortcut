b0a68f77b3db66d7eac476e183812c676a1067d2
Paul Woo Jung Kim <paulwoojungkim@Paului-MacBookPro.local>
2020-08-14 00:01:11 +0900

    final test

--- popup.js
+++ popup.js
@@ -4,44 +4,46 @@ iframe link url : google doc - 알서포트 업무링크 라이브러리
 const link = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSTunQ8bnaysSFF_oFf2hHxZE0Ubfpd6-xKH9p5a2UGMBrtFRxFuyRAdNh42kb06ETZcZpG35pLvWCI/pubhtml?widget=true&amp;headers=false';
 const initialLoadingStatus = true;
 
-const createState = (state) => {
-    return new Proxy(state, {
-      set(target, property, value) {
-        target[property] = value; 
-        render(); 
-        return true;
-  }
-    });
-  };
+// const createState = (state) => {
+//     return new Proxy(state, {
+//       set(target, property, value) {
+//         target[property] = value; 
+//         render(); 
+//         return true;
+//   }
+//     });
+//   };
 
-const state = createState({
-isloading:initialLoadingStatus
-});
+// const state = createState({
+// isloading:initialLoadingStatus
+// });
 
-const render = () => {
-    if(!state.isloading){
-        const loaderWrapper = document.querySelector('.loader-wrapper');
-        loaderWrapper.parentElement.firstElementChild.remove();
-        // loaderWrapper.style.display="none"; // display: none might hold up it's resources?
-    }
-};
+// const render = () => {
+//     if(!state.isloading){
+//         const loaderWrapper = document.querySelector('.loader-wrapper');
+//         loaderWrapper.parentElement.firstElementChild.remove();
+//         // loaderWrapper.style.display="none"; // display: none might hold up it's resources?
+//     }
+// };
 
 // render();
 
 const iframe = document.createElement("iframe");
 iframe.className ="link-library";  
 iframe.style.display = "none";
+iframe.src = link;
+iframe.setAttribute("loading","lazy"); // page appears before iframe loads
+
 
 const libraryWrapper = document.querySelector('.link-library-wrapper');
 libraryWrapper.appendChild(iframe);
 
 iframe.onload = () => {
-    iframe.setAttribute("loading","lazy"); // page appears before iframe loads
-    state.isloading = false;
+    // state.isloading = false;
+    iframe.style.display="block";
 };
 
-iframe.src = link;
-iframe.style.display="block";
+
 
 
 
