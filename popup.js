097dbb81da34c8887c31bc9a9d02953640b9b7ac
Paul Woo Jung Kim <paulwoojungkim@Paului-MacBookPro.local>
2020-08-13 15:36:01 +0900

    version -> 1.0.0

--- popup.js
+++ popup.js
@@ -2,7 +2,6 @@
 iframe link url : google doc - 알서포트 업무링크 라이브러리
 */
 const link = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSTunQ8bnaysSFF_oFf2hHxZE0Ubfpd6-xKH9p5a2UGMBrtFRxFuyRAdNh42kb06ETZcZpG35pLvWCI/pubhtml?widget=true&amp;headers=false';
-
 const initialLoadingStatus = true;
 
 const createState = (state) => {
@@ -22,26 +21,28 @@ isloading:initialLoadingStatus
 const render = () => {
     if(!state.isloading){
         const loaderWrapper = document.querySelector('.loader-wrapper');
-        loaderWrapper.style.display="none";
+        loaderWrapper.parentElement.firstElementChild.remove();
+        // loaderWrapper.style.display="none"; // display: none might hold up it's resources?
     }
 };
 
-const iframe = document.createElement("iframe");
-const libraryWrapper = document.querySelector('.link-library-wrapper');
+// render();
 
+const iframe = document.createElement("iframe");
+iframe.className ="link-library";  
 iframe.style.display = "none";
+
+const libraryWrapper = document.querySelector('.link-library-wrapper');
 libraryWrapper.appendChild(iframe);
 
 iframe.onload = () => {
     iframe.setAttribute("loading","lazy"); // page appears before iframe loads
-    iframe.className ="link-library";  
     state.isloading = false;
-    iframe.style.display="block";
 };
 
 iframe.src = link;
+iframe.style.display="block";
 
-render();
 
 
 
